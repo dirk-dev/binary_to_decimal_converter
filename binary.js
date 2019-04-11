a53fa2f50c50f53dc5da28b4e61d9ba44e38d923
Dirk Kiesewetter <dirk.k1@outlook.com>
2019-04-11 13:40:23 -0400

    fixed regex to filter out nonbinary #s, limited input to 16 characters

--- binary.js
+++ binary.js
@@ -1,28 +1,25 @@
 const submitButton = document.querySelector('#binarySubmit')
-const regex = /[0-1]/
 
 submitButton.onclick = function () {
   event.preventDefault()
   let binaryNum = document.querySelector('#binaryNum').value
+  const regex = /^[0|1]{1,}$/
   // checks that user input only binary digits
   if (regex.test(binaryNum)) {
     let binaryNum = document.getElementById('binaryNum').value
     console.log(binaryNum)
-  } else {
-    alert('Please enter the correct input.')
-  }
-  // console.log(binaryNum.length)
-  let decimalNumber = 0
-  let numberMultiplier = 1
-  for (let i = binaryNum.length - 1; i >= 0; i--) {
-    console.log(`iteration ${i}: ${binaryNum.charAt(i)}`)
-    if (binaryNum.charAt(i) == 1) {
-      decimalNumber = decimalNumber + numberMultiplier
+    let decimalNumber = 0
+    let numberMultiplier = 1
+    for (let i = binaryNum.length - 1; i >= 0; i--) {
+      if (binaryNum.charAt(i) == 1) {
+        decimalNumber = decimalNumber + numberMultiplier
+      }
+      numberMultiplier = numberMultiplier * 2
     }
-    numberMultiplier = numberMultiplier * 2
-    console.log('numberMultiplier:', numberMultiplier)
-    console.log('------------------')
+    document.querySelector('#decimalNum').innerHTML = decimalNumber
+  } else if (!binaryNum) {
+    alert('Please enter a binary number.')
+  } else if (binaryNum != regex) {
+    alert('Please enter binary digits only.')
   }
-  console.log('decimal number:', decimalNumber)
-  document.querySelector('#decimalNum').innerHTML = decimalNumber
 }

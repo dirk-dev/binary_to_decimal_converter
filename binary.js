477ddbd10f93fa7925dc066e1fcf08311cd4af00
Dirk Kiesewetter <dirk.k1@outlook.com>
2019-04-27 17:39:46 -0400

    adding code for decimal to bainry converter - intial commit

--- binary.js
+++ binary.js
@@ -1,6 +1,7 @@
-const submitButton = document.querySelector('#binarySubmit')
+const submitButtonBinary = document.querySelector('#binarySubmit')
+const submitButtonDecimal = document.querySelector('#decimalSubmit')
 
-submitButton.onclick = function () {
+submitButtonBinary.onclick = function () {
   event.preventDefault()
   let binaryNum = document.querySelector('#binaryNum').value
   const regex = /^[0|1]{1,}$/
@@ -10,7 +11,7 @@ submitButton.onclick = function () {
     let decimalNumber = 0
     let numberMultiplier = 1
     for (let i = binaryNum.length - 1; i >= 0; i--) {
-      if (binaryNum.charAt(i) == 1) {
+      if (binaryNum.charAt(i) === 1) {
         decimalNumber = decimalNumber + numberMultiplier
       }
       numberMultiplier = numberMultiplier * 2
